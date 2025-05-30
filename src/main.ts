314efcb879622e5bcce94f64c3c40bebaf63f885
Robert <trangbn580@gmail.com>
2025-05-30 17:22:12 +0700

    Add ci/cd api-users

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new ResponseTransformInterceptor())
   app.useGlobalPipes(new ValidationPipe(ValidationConfig))
 
-  app.getHttpAdapter().get('/', (req, res) => {
-    res.status(200).send({ status: 'ok' });
-  });
-
   app.setGlobalPrefix(configService.get<string>('apiPrefix'))
 
   useContainer(app.select(ValidatorModule), { fallbackOnErrors: true })
